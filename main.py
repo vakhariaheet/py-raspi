e1103eb38a8fcc1c846df86e75f0903bb436163a
Heet Vakharia <heetkv@gmail.com>
2024-10-27 00:38:26 +0530

    Testing RGB

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from sensors.temperature import DHT11;
 from sensors.camera import CameraSensor;
 from services.gemini import GeminiHandler
 import pygame;
+import os;
 import time;
 dht11 = DHT11(pin=27);
 pygame.init()
@@ -13,7 +14,8 @@ def main():
             result = dht11.read();
             print(str(result));
             CameraSensor().capture("image.jpg");
-            gemini = GeminiHandler();
+            print("Image captured");
+            gemini = GeminiHandler(api_key=os.getenv('API_KEY'));
             gemini.generate_with_tts("Describe this scene as if narrating to someone who can't see it. Be detailed but natural, avoiding any mention of an image. Use only elements present in the scene. Keep your description concise, under 100 words, while capturing the essence of what's visible.",image_path="image.jpg");
             print("Text to speech completed");
         print(f'Touch Detected {props}');
