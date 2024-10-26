ff5927843f5ad280b1cb7ea5e55649790571f83d
Heet Vakharia <heetkv@gmail.com>
2024-10-27 03:19:39 +0530

    Testing RGB

--- sensors/temperature.py
+++ sensors/temperature.py
@@ -81,3 +81,8 @@ class DHT11Sensor:
             return (temp * 9/5) + 32
         return None
     
+    def __del__(self):
+        """
+        Clean up resources when object is deleted
+        """
+        self.dht_device.exit()
